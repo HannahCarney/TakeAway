44b054235733704beff12e16f3858caed64a1a22
Hannah Carney <hannahcarneyart@gmail.com>
2014-12-30 19:02:56 +0000

    added Thank you for ordering message

--- TakeAway.rb
+++ TakeAway.rb
@@ -1,18 +1,22 @@
-$dishes = {"Soup" => 4.5, "Rice" => 2, "Pizza" => 8, "Curry" => 8, "Salad" => 5, "Chicken" => 4, "Beef" => 6}
 
 @items = []
 
 class Order
-
+  
+ $dishes = {"Soup" => 4.5, "Rice" => 2, "Pizza" => 8, "Curry" => 8, "Salad" => 5, "Chicken" => 4, "Beef" => 6}
+  
+ 
   def initialize
-
+   
   end
   
-  def order_now
-    puts "Please pick an item or items from the menu by typing the menu item"
-    items = gets.chomp
-    puts "If you are finished ordering then type 'Finished' otherwise type in another item"
-    quit = gets.chomp
+
+  def ordered
+    @items = []
+  end
+
+  def item_count
+    @items.count
   end
 
   def error
@@ -22,26 +26,24 @@ class Order
   def order_placed
     puts "Thank you! Your order was placed and will be delivered before 18:52."
   end
-
 end
 
+order = Order.new
   puts "Hello, and welcome to TakeAway! Here is the menu:"
   $dishes.each {|key, value| puts "#{key}: $#{value}"}
     puts "Please pick an item or items from the menu by typing the menu item"
-    items = gets.chomp
+    items = gets.chomp.capitalize
     begin
       if $dishes.has_key?(items)
         puts "Order for #{items} has been placed. Would you like to order anything else?"
         @items << items
-        items = gets.chomp
+        items = gets.chomp.capitalize
       else 
         puts "Sorry! Not a correct item from the menu. Enter one, or enter 'Finished' if you are done"
-        items = gets.chomp
+        items = gets.chomp.capitalize
       end
     end until items.capitalize == "Finished"
-
-
-  
-puts @items.inspect
+     puts "Thank you for ordering! You now have ordered #{@items.count} items:"  
+      @items.each {|item| puts "#{item} "}
 
 
