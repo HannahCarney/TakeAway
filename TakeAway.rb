26bf939baac4055f17e438a7efee678e9b293833
Hannah Carney <hannahcarneyart@gmail.com>
2014-12-30 23:39:32 +0000

    Tells you what amount of foods you ordered

--- TakeAway.rb
+++ TakeAway.rb
@@ -6,57 +6,57 @@ class Order
  @@quantity = 0
 
 
-
-
-  
-  
-  def delivery_time
-      puts "Thank you for ordering! Your Food will be delivered before #{Time.new.hour + 1 > 12 ? (Time.new.hour + 1) - 12 : Time.new.hour + 1}:#{Time.new.min < 10 ? (sprintf '%02d', Time.new.min).to_i : Time.new.min} #{Time.new.hour > 12 ? 'PM' : 'AM'} (GMT)."
+  def start_statement
+    puts "Hello, and welcome to TakeAway! Here is the menu:"
+    $dishes.each {|key, value| puts "#{key}: $#{value}"}
+    puts "Please pick an item from the menu by typing the menu item"
   end
 
   def ordered
-    @items ||= [] 
+    @quantity ||= []
+    @items ||= []
    end
 
   def item_count
      @items.nil? ? 0 : @items.count
   end
 
-  def add_item(items)
+  def add_item(items, amount)
+    ordered << amount
     ordered << items
+    
   end
 
-  def quantity(items)
-    if $dishes.keys.include?(items)
-      @@quantity += 1
-    end
+
+  def delivery_time
+      puts "Thank you for ordering! Your Food will be delivered before #{Time.new.hour + 1 > 12 ? (Time.new.hour + 1) - 12 : Time.new.hour + 1}:#{Time.new.min < 10 ? (sprintf '%02d', Time.new.min).to_i : Time.new.min} #{Time.new.hour > 12 ? 'PM' : 'AM'} (GMT)."
   end
 
   def end_statement
-     puts "#{self.delivery_time}You have ordered #{self.item_count} items:"  
-     self.ordered.each {|item| puts "#{item} "}
+     puts "#{self.delivery_time}You have ordered #{self.item_count} categories of food:"  
+
+     self.ordered.each {|quantity, item| puts "#{quantity} #{item}"}
+     
   end
     
 end
 
 order = Order.new
-  puts "Hello, and welcome to TakeAway! Here is the menu:"
-  $dishes.each {|key, value| puts "#{key}: $#{value}"}
-    puts "Please pick an item from the menu by typing the menu item"
-    items = gets.chomp.capitalize
+   order.start_statement
+   items = gets.chomp.capitalize
     begin
       if $dishes.has_key?(items)
         puts "How many of those would you like?"
-        quantity = gets.to_i
+        amount = gets.to_i
         begin
-        if quantity.is_a?(Integer) && quantity > 0
+        if amount.is_a?(Integer) && amount > 0
         else
           puts "Please enter a digit that is a valid quantity for #{items}"
-          quantity = gets.to_i
+          amount = gets.to_i
         end
-      end until quantity.is_a?(Integer) && quantity > 0
-        puts "Order for #{quantity} #{items} has been placed. Would you like to order anything else?"
-        order.add_item(items)
+      end until amount.is_a?(Integer) && amount > 0
+        puts "Order for #{amount} #{items} has been placed. Would you like to order anything else?"
+        order.add_item(items, amount)
         items = gets.chomp.capitalize
       else 
         puts "Sorry! Not a correct item from the menu. Enter one, or enter 'Finished' if you are done"
