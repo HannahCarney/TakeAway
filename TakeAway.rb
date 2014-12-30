2fc08f911456b79942da3f3c000c55d64d0cf59d
Hannah Carney <hannahcarneyart@gmail.com>
2014-12-30 20:46:02 +0000

    Refactored code

--- TakeAway.rb
+++ TakeAway.rb
@@ -1,24 +1,24 @@
-@items = []
+
 
 class Order
   
  $dishes = {"Soup" => 4.5, "Rice" => 2, "Pizza" => 8, "Curry" => 8, "Salad" => 5, "Chicken" => 4, "Beef" => 6}
   
- 
-  def initialize
-   
-  end
   
   def delivery_time
       puts "Thank you for ordering! Your Food will be delivered before #{Time.new.hour + 1 > 12 ? (Time.new.hour + 1) - 12 : Time.new.hour + 1}:#{Time.new.min} #{Time.new.hour > 12 ? 'PM' : 'AM'} (GMT)."
   end
 
   def ordered
-    @items = []
-  end
+    @items ||= [] 
+   end
 
   def item_count
     @items.count
+  end
+
+  def add_item(items)
+    ordered << items
   end
 
   def error
@@ -38,13 +38,13 @@ order = Order.new
     begin
       if $dishes.has_key?(items)
         puts "Order for #{items} has been placed. Would you like to order anything else?"
-        @items << items
+        order.add_item(items)
         items = gets.chomp.capitalize
       else 
         puts "Sorry! Not a correct item from the menu. Enter one, or enter 'Finished' if you are done"
         items = gets.chomp.capitalize
       end
     end until items.capitalize == "Finished"
-     puts "#{order.delivery_time} You now have ordered #{@items.count} items:"  
-      @items.each {|item| puts "#{item} "}
+     puts "#{order.delivery_time}You have ordered #{order.item_count} items:"  
+      order.ordered.each {|item| puts "#{item} "}
 
